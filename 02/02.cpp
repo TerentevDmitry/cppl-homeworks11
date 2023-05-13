3a0f2a7a86c431167cf613cb35d667765d7d26bc
TerentevDmitry <Zabey52@gmail.com>
2023-05-13 19:03:05 +0300

    z1v1 - add std::move

---
+++ 02/02.cpp
@@ -0,0 +1,25 @@
+﻿// Домашнее задание к занятию «Move семантика»
+// Задача 2. Большие числа
+
+#include "BigInteger.h"
+
+int main()
+{
+    
+    auto number0 = BigInteger("31");
+    std::cout << number0.getSize() << std::endl;
+
+    auto number3 = BigInteger("34");
+    std::cout << number0.getSize() << std::endl;
+
+    //auto number1 = BigInteger("114575");
+    //std::cout << number1.getSize() << std::endl;
+
+    //auto number2 = BigInteger("78524");
+    //std::cout << number2.getSize() << std::endl;
+
+    auto result = number0 + number3;
+    std::cout << "193099 55 = number1 + number2 = " << result; // 193099
+
+
+}
